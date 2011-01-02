d380c24092417dad762b6b217a9744d6687a477e
Marcin Wiśniowski <shfx@shfx.pl>
2011-01-02 14:46:00 +0100

    plugins, etc.

---
+++ elpa/ruby-test-mode-1.0/ruby-test-mode-autoloads.el
@@ -0,0 +1,30 @@
+;;; ruby-test-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ruby-test-mode) "ruby-test-mode" "ruby-test-mode.el"
+;;;;;;  (19738 36583))
+;;; Generated autoloads from ruby-test-mode.el
+
+(autoload (quote ruby-test-mode) "ruby-test-mode" "\
+Toggle Ruby-Test minor mode.
+With no argument, this command toggles the mode. Non-null prefix
+argument turns on the mode. Null prefix argument turns off the
+mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ruby-test-mode-pkg.el") (19738 36583
+;;;;;;  630034))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ruby-test-mode-autoloads.el ends here
