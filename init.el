4cac84948ff4e4c85e42ff96765a534eaed98762
Marcin Wiśniowski <shfx@shfx.pl>
2014-11-01 22:50:41 +0100

    Modified init.el and added elpa to .gitignore

--- init.el
+++ init.el
@@ -1,3 +1,13 @@
+(require 'dash)
+(require 's)
+
+;; require ido
+(require 'ido)
+(ido-mode t)
+
+;; require magit
+(require 'magit)
+
 ;; Startup messages
 
 (setq inhibit-startup-message t)
@@ -9,20 +19,10 @@
 (prefer-coding-system 'utf-8)
 
 ;; bell
-(setq visible-bell 'top-bottom)
-(setq ring-bell-function 'ignore)
-
-;(setq ring-bell-function
-;      (lambda ()
-;        (unless (memq this-command
-;                      '(isearch-abort abort-recursive-edit exit-minibuffer keyboard-quit))
-;          (ding))))
-
-;; Paths
-(setq emacsdir "~/.emacs.d")                    ; main config dir
-(setq elpa-dir (concat emacsdir "/elpa"))       ; elpa packages dir
-(setq plugins-dir (concat emacsdir "/plugins")) ; not-elpa'd plugins
-(setq theme-dir (concat emacsdir "/themes"))
+(setq visible-bell 'top-bottom
+      ring-bell-function 'ignore
+      ;; Paths
+      emacsdir "~/.emacs.d")
 
 ;; custom file
 (setq custom-file (concat emacsdir "/custom.el"))
@@ -31,65 +31,47 @@
 (load (concat emacsdir "/functions.el"))
 (load (concat emacsdir "/keys.el"))
 
-(add-to-list 'load-path plugins-dir)            ; set up plugins path
+(tool-bar-mode 0)
+(menu-bar-mode 0)
 
-(setq features
-      '((ido       . t)
-        (tramp     . t)
-        (php-mode  . t)
-        (js3-mode  . t)
-        (font-lock . t)
-        (elip      . t)
-        (theming   . window-system)
-        (toolbar   . window-system)))
+(setq initial-frame-alist
+      '((menu-bar-lines . 0)
+        (tool-bar-lines . 0)))
 
-(feature ido
-         (require 'ido)
-         (ido-mode t)
-         (setq ido-create-new-buffer 'always)
-         (setq ido-enable-flex-matching t))
-
-(feature tramp
-         (require 'tramp))
-
-(feature php-mode
-         (require 'php-mode)
-         (add-to-list 'auto-mode-alist '("\\.php\\'\\|\\.phtml\\'" . php-mode)))
-
-(feature js3-mode
-	 (autoload 'js3-mode "js3" nil t)
-         ;(require 'js3)
-         (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode)))
- 
-(feature font-lock
-         (require 'font-lock)
-         (global-font-lock-mode t))
-
-(feature elip
-         (when
-             (load (expand-file-name (concat elpa-dir "/package.el")))
-           (package-initialize)))
-
-(feature theming
-         (add-to-list 'custom-theme-load-path theme-dir)
-         (load-theme 'tango-dark))
-
-(feature toolbar
-         (tool-bar-mode -1))
-
+(global-hl-line-mode 0)
+(setq pop-up-frames nil)
+(set-window-margins nil 0 0)
+(electric-pair-mode 1)
+(show-paren-mode 1)
+(global-linum-mode 1)
+(scroll-bar-mode 0)
 (line-number-mode t)
 (column-number-mode t)
                                         ; podswietlanie zaznaczenia
 (setq transient-mark-mode t
-      ;delete-selection-mode t
       show-paren-mode 1
       delete-key-deletes-forward t
-      mouse-yank-at-point t)
+      mouse-yank-at-point t
+      make-backup-files nil
+      auto-save-default nil)
 
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 2)
 (setq-default tab-width 2)
+(fset 'yes-or-no-p 'y-or-n-p)
 
-(fset 'yes-or-no-p 'y-or-n-p) ; zamienia prompty yes/no na y/n
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (push '("<=" . ?≤) prettify-symbols-alist)))
+
+(-each
+   (-map
+      (lambda (item)
+      (format "~/.emacs.d/elpa/%s" item))
+   (-filter
+      (lambda (item) (s-contains? "theme" item))
+      (directory-files "~/.emacs.d/elpa/")))
+   (lambda (item)
+      (add-to-list 'custom-theme-load-path item)))
 
 (message ".emacs loaded successfully.")
