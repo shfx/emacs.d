79514298c1896d7460f7074257dccf2463ce9c10
Marcin Wiśniowski <shfx@shfx.pl>
2014-11-01 22:47:14 +0100

    Updating my emacs config

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("fa942713c74b5ad27893e72ed8dccf791c9d39e5e7336e52d76e7125bfa51d4c" default)))
  '(js3-allow-keywords-as-property-names nil)
  '(js3-auto-indent-p t)
  '(js3-consistent-level-indent-inner-bracket t)
@@ -15,7 +18,11 @@
  '(js3-lazy-operators t)
  '(js3-mirror-mode t)
  '(js3-paren-indent-offset 2)
- '(js3-square-indent-offset 2))
+ '(js3-square-indent-offset 2)
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
