d8dbf1eb76a0f8a5ec6ec941819c0f62191b1aa1
Marcin Wiśniowski <shfx@shfx.pl>
2021-03-24 23:58:42 +0100

    fixup! 20210324

--- init.el
+++ init.el
@@ -1,49 +1,46 @@
-(let ((gc-cons-threshold most-positive-fixnum))
-  (setq-default package-enable-at-startup nil)
+;; Packages
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                          ("melpa-stable" . "https://stable.melpa.org/packages/")
+                          ("org" . "https://orgmode.org/elpa/")
+                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
-  ;; Packages
-  (require 'package)
-  (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
 
-  (package-initialize)
+;; Package management
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-  ;; Package management
-  (unless (package-installed-p 'use-package)
-    (package-install 'use-package))
+;; All packages should be ensured unless stated othewise
+(setq-default use-package-always-ensure t)
 
-  ;; All packages should be ensured unless stated othewise
-  (setq-default use-package-always-ensure t)
+;; Garbage Collector Magic Hack
+(use-package gcmh
+  :ensure t
+  :config
+  (gcmh-mode 1))
 
-  ;; Garbage Collector Magic Hack
-  (use-package gcmh
-    :ensure t
-    :config
-    (gcmh-mode 1))
+;; Install newer version of org-mode
+(use-package org
+  :mode (("\\.org$" . org-mode))
+  :ensure org-plus-contrib)
 
-  ;; Install newer version of org-mode
-  (use-package org
-    :mode (("\\.org$" . org-mode))
-    :ensure org-plus-contrib)
+;; ;; Track max-specpdl-size exceded error by uncommenting this
+;; (setq max-specpdl-size 5)
+;; (setq debug-on-error t)
 
-  ;; ;; Track max-specpdl-size exceded error by uncommenting this
-  ;; (setq max-specpdl-size 5)
-  ;; (setq debug-on-error t)
+;; Load org literal config config
+(org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 
-  ;; Load org literal config config
-  (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
+(setq custom-file
+  (if (boundp 'server-socket-dir)
+    (expand-file-name "custom.el" server-socket-dir)
+    (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
 
-  (setq custom-file
-    (if (boundp 'server-socket-dir)
-      (expand-file-name "custom.el" server-socket-dir)
-      (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
+(load custom-file t)
 
-  (load custom-file t)
-
-  (garbage-collect))
+(garbage-collect)
 
 (provide 'init)
 ;;; init.el ends here
-(put 'upcase-region 'disabled nil)
