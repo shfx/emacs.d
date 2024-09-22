8cca9e48e7395dd97982775496c5cebc98741d91
shfx <opera_user@laptopoperauser.home>
2024-09-22 11:52:42 +0200

    feat: add elpaca package manager

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t;-*-
+
 (setopt custom-file
         (if (boundp 'server-socket-dir)
             (expand-file-name "custom.el" server-socket-dir)
@@ -15,30 +17,61 @@
 
 ;; Packages
 
-(package-initialize)
+(defvar elpaca-installer-version 0.7)
+(defvar elpaca-directory
+  (locate-user-emacs-file
+   (concat
+    (file-name-as-directory "var/elpaca")
+    emacs-version)))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 
-(eval-when-compile
-  (require 'use-package))
+(defvar elpaca-order
+  '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+           :ref nil :depth 1
+           :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+           :build (:not elpaca--activate-package)))
+
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                 ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                 ,@(when-let ((depth (plist-get order :depth)))
+                                                     (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                 ,(plist-get order :repo) ,repo))))
+                 ((zerop (call-process "git" nil buffer t "checkout"
+                                       (or (plist-get order :ref) "--"))))
+                 (emacs (concat invocation-directory invocation-name))
+                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                 ((require 'elpaca))
+                 ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+
+(add-hook 'after-init-hook #'elpaca-process-queues)
+
+(require 'use-package)
+
+(elpaca `(,@elpaca-order))
+
+(elpaca elpaca-use-package
+  (elpaca-use-package-mode))
 
 (setq use-package-always-ensure t)
-
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("gnu" . "https://elpa.gnu.org/packages/")))
-
-(if (not package-archive-contents)
-    (progn
-      (message "Refreshing content")
-      (package-refresh-contents))
-  (progn
-    (message "Refreshing content async")
-    (package-refresh-contents t)))
-
-(use-package use-package-ensure-system-package
-  :ensure t
-  :custom
-  (async-shell-command-buffer 'new-buffer))
+(setq use-package-verbose t);
 
 (use-package no-littering)
 
@@ -69,24 +102,11 @@
             (daemonp))
     (exec-path-from-shell-initialize)))
 
-;; Install newer version of org-mode
-(use-package org
-  :pin gnu
-  :commands (org-mode org-capture org-agenda)
-  :mode (("\\.org$" . org-mode)))
-
-(use-package org-contrib
-  :pin nongnu
-  :after org)
-
 ;; ;; Track max-specpdl-size exceded error by uncommenting this
 ;; (setq max-specpdl-size 5)
 ;; (setq debug-on-error t)
 
 ;; Load org literal config config
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
-
-(load custom-file t)
-(put 'narrow-to-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
+(add-hook 'elpaca-after-init-hook (lambda ()
+                                    (load custom-file 'noerror 'nomessage)))
