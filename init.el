ab7fb7f09ea144893183990d0b0562798788cd40
Marcin Wiśniowski <shfx@shfx.pl>
2026-07-11 19:25:02 +0200

    refactor: replace better defaults and some refactor

--- init.el
+++ init.el
@@ -1,115 +1,117 @@
 ;;; -*- lexical-binding: t -*-
 
+(setopt custom-file (make-temp-file "emacs-custom"))
+
 ;; ;; Track max-specpdl-size exceded error by uncommenting this
 ;; (setq max-specpdl-size 5)
 ;; (setq debug-on-error t)
 
 (setopt
- disabled-command-function nil
- ad-redefinition-action 'accept
- auto-revert-avoid-polling t
- auto-revert-stop-on-user-input nil
- auto-save-default t
- auto-save-include-big-deletions t
- auto-window-vscroll nil
- backup-by-copying t
- backup-by-copying-when-linked t
- column-number-mode t
- comint-buffer-maximum-size 2048
- comint-prompt-read-only t
- comment-empty-lines t
- comment-multi-line t
- compilation-always-kill t
- compilation-ask-about-save nil
- compilation-scroll-output 'first-error
- confirm-nonexistent-file-or-buffer nil
- create-lockfiles nil
- cursor-in-non-selected-windows nil
- delete-old-versions t
- delete-pair-blink-delay 0.03
- dired-clean-confirm-killing-deleted-buffers nil
- dired-create-destination-dirs 'ask
- dired-recursive-copies 'always
- dired-recursive-deletes 'top
- display-line-numbers-width 3
- display-time-default-load-average nil
- ediff-split-window-function #'split-window-horizontally
- ediff-window-setup-function #'ediff-setup-windows-plain
- eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly
- ffap-machine-p-known 'reject
- find-file-suppress-same-file-warnings t
- find-file-visit-truename t
- frame-resize-pixelwise t
- global-text-scale-adjust-resizes-frames nil
- highlight-nonselected-windows nil
- hscroll-margin 2
- hscroll-step 1
- idle-update-delay 0.5
- indicate-empty-lines nil
- kept-new-versions 5
- kept-old-versions 5
- kill-buffer-delete-auto-save-files t
- kill-do-not-save-duplicates t
- lazy-highlight-initial-delay 0
- mac-command-modifier 'super
- mac-option-modifier 'meta
- make-backup-files nil
- mouse-wheel-flip-direction t
- mouse-wheel-scroll-amount '(1 ((shift) . hscroll))
- mouse-wheel-scroll-amount-horizontal 1
- mouse-wheel-tilt-scroll t
- mouse-yank-at-point t
- recentf-auto-cleanup 'mode
- recentf-max-saved-items 300
- resize-mini-windows 'grow-only
- revert-without-query (list ".")
- ring-bell-function #'ignore
- scroll-conservatively 0
- scroll-error-top-bottom t
- scroll-margin 15
- scroll-preserve-screen-position t
- scroll-step 1
- sentence-end-double-space nil
- sh-indent-after-continuation 'always
- show-paren-delay 0.1
- show-paren-highlight-openparen t
- show-paren-when-point-in-periphery t
- show-paren-when-point-inside-paren t
- switch-to-buffer-obey-display-actions t
- truncate-lines t
- truncate-partial-width-windows nil
- truncate-string-ellipsis "…"
- uniquify-buffer-name-style 'forward
- vc-follow-symlinks t
- vc-make-backup-files nil
- version-control t
- visible-bell nil
- warning-suppress-types '((lexical-binding))
- window-divider-default-bottom-width 1
- window-divider-default-places t
- window-divider-default-right-width 1
- window-resize-pixelwise nil
- x-underline-at-descent-line nil)
+  disabled-command-function nil
+  ad-redefinition-action 'accept
+  auto-revert-avoid-polling t
+  auto-revert-stop-on-user-input nil
+  auto-save-default t
+  auto-save-include-big-deletions t
+  auto-window-vscroll nil
+  backup-by-copying t
+  backup-by-copying-when-linked t
+  column-number-mode t
+  comint-buffer-maximum-size 2048
+  comint-prompt-read-only t
+  comment-empty-lines t
+  comment-multi-line t
+  compilation-always-kill t
+  compilation-ask-about-save nil
+  compilation-scroll-output 'first-error
+  confirm-nonexistent-file-or-buffer nil
+  create-lockfiles nil
+  cursor-in-non-selected-windows nil
+  delete-old-versions t
+  delete-pair-blink-delay 0.03
+  dired-clean-confirm-killing-deleted-buffers nil
+  dired-create-destination-dirs 'ask
+  dired-recursive-copies 'always
+  dired-recursive-deletes 'top
+  display-line-numbers-width 3
+  display-time-default-load-average nil
+  ediff-split-window-function #'split-window-horizontally
+  ediff-window-setup-function #'ediff-setup-windows-plain
+  eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly
+  ffap-machine-p-known 'reject
+  find-file-suppress-same-file-warnings t
+  find-file-visit-truename t
+  frame-resize-pixelwise t
+  global-text-scale-adjust-resizes-frames nil
+  highlight-nonselected-windows nil
+  hscroll-margin 2
+  hscroll-step 1
+  idle-update-delay 0.5
+  indicate-empty-lines nil
+  kept-new-versions 5
+  kept-old-versions 5
+  kill-buffer-delete-auto-save-files t
+  kill-do-not-save-duplicates t
+  lazy-highlight-initial-delay 0
+  mac-command-modifier 'super
+  mac-option-modifier 'meta
+  make-backup-files nil
+  mouse-wheel-flip-direction t
+  mouse-wheel-scroll-amount '(1 ((shift) . hscroll))
+  mouse-wheel-scroll-amount-horizontal 1
+  mouse-wheel-tilt-scroll t
+  mouse-yank-at-point t
+  recentf-auto-cleanup 'mode
+  recentf-max-saved-items 300
+  resize-mini-windows 'grow-only
+  revert-without-query (list ".")
+  ring-bell-function #'ignore
+  scroll-conservatively 0
+  scroll-error-top-bottom t
+  scroll-margin 15
+  scroll-preserve-screen-position t
+  scroll-step 1
+  sentence-end-double-space nil
+  sh-indent-after-continuation 'always
+  show-paren-delay 0.1
+  show-paren-highlight-openparen t
+  show-paren-when-point-in-periphery t
+  show-paren-when-point-inside-paren t
+  switch-to-buffer-obey-display-actions t
+  truncate-lines t
+  truncate-partial-width-windows nil
+  truncate-string-ellipsis "…"
+  uniquify-buffer-name-style 'forward
+  vc-follow-symlinks t
+  vc-make-backup-files nil
+  version-control t
+  visible-bell nil
+  warning-suppress-types '((lexical-binding))
+  window-divider-default-bottom-width 1
+  window-divider-default-places t
+  window-divider-default-right-width 1
+  window-resize-pixelwise nil
+  x-underline-at-descent-line nil)
 
 (add-hook 'after-init-hook #'window-divider-mode)
 
 (if (fboundp #'json-parse-string)
-    (push 'jansson features))
+  (push 'jansson features))
 
 (if (string-match-p "HARFBUZZ" system-configuration-features)
-    (push 'harfbuzz features))
+  (push 'harfbuzz features))
 
 (if (bound-and-true-p module-file-suffix)
-    (push 'dynamic-modules features))
+  (push 'dynamic-modules features))
 
 (setopt minibuffer-prompt-properties
-        '(read-only t intangible t cursor-intangible t face
-                    minibuffer-prompt))
+  '(read-only t intangible t cursor-intangible t face
+     minibuffer-prompt))
 
 (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
 (if (boundp 'use-short-answers)
-    (setq use-short-answers t)
+  (setq use-short-answers t)
   (advice-add #'yes-or-no-p :override #'y-or-n-p))
 (defalias #'view-hello-file #'ignore)  ; Never show the hello file
 
@@ -147,7 +149,7 @@
   (exec-path-from-shell-arguments '("-l"))
   :config
   (when (or (memq window-system '(mac ns x))
-            (daemonp))
+          (daemonp))
     (exec-path-from-shell-initialize)))
 
 (elpaca-wait)
